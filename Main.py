cc2f295c31231e96908787a9f6454fbafbebf035
ultratroll <josejoeldejesus@gmail.com>
2020-06-28 13:46:14 -0500

    creating small rogueEngine class, to handle rendering and input!

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from rogueEngine import Engine
 from entity import Entity
 from input_handlers import EventHandler
 
@@ -20,6 +20,8 @@ def main() -> None:
 	npc= Entity(int(screen_width/2)-5, int(screen_height/2)-5, "@", (0,255,0))
 
 	entities= {npc, player}
+	
+	engine= Engine(entities= entities, event_handler= event_handler, player= player)
 
 	with tcod.context.new_terminal(
 		screen_width,
@@ -30,23 +32,11 @@ def main() -> None:
 	) as context:
 		root_console = tcod.Console(screen_width, screen_height, order="F")
 		while True:
-			root_console.print(x=player.x, y=player.y, string=player.char, fg= player.color)
-			
-			context.present(root_console)
-
-			root_console.clear()
-
-			for event in tcod.event.wait():
-				action= event_handler.dispatch(event)
-				
-				if action is None:
-					continue
-					
-				if isinstance(action, MovementAction):
-					player.move(dx=action.dx, dy=action.dy)
-					
-				elif isinstance(action, EscapeAction):
-					raise SystemExit()
+			engine.render(console= root_console, context= context)
+		
+			events = tcod.event.wait()
+		
+			engine.handle_events(events)
 
 
 if __name__ == "__main__":
