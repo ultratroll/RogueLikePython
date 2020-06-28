3d7fac6d3ea8f4bf6dfe724f9fc0e0fe4624fe90
ultratroll <josejoeldejesus@gmail.com>
2020-06-28 14:32:56 -0500

    huge and useful refactor
    Rather than having the engine do the action logic depending on the action type, now to actions we pass the engine and then the action must implement what logic is going to perform
    
    much more cleaner and easier to read and mantain !

--- rogueEngine.py
+++ rogueEngine.py
@@ -3,7 +3,6 @@ from typing import Set, Iterable, Any
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -24,13 +23,8 @@ class Engine:
 			if action is None:
 				continue
 			
-			if isinstance(action, MovementAction):
-				# now lets only walk if we can
-				if self.game_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy]:
-					self.player.move(dx=action.dx, dy=action.dy)
-				
-			elif isinstance(action, EscapeAction):
-				raise SystemExit()
+			# now for the resulting actiosn from input (movement, quitting etc), the action must perform its own logic!
+			action.perform(self, self.player)
 				
 	# Just to draw
 	def render(self, console: Console, context: Context) -> None:
