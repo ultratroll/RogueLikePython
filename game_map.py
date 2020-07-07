b6a325a97ec12ae939495bf60c4946feed3fe957
ultratroll <josejoeldejesus@gmail.com>
2020-07-07 08:58:28 -0500

    now we have a bump action that handles movements or attacks
    attacks currently just print a msg to the terminal

--- game_map.py
+++ game_map.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, Optional, TYPE_CHECKING
 
 import numpy as np  # type: ignore
 from tcod.console import Console
@@ -18,6 +18,13 @@ class GameMap:
 		self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 		self.visible = np.full((width, height), fill_value= False, order= "F") # tiles the player can see
 		self.explored = np.full((width, height), fill_value= False, order= "F") # tiles the player has explored
+		
+	def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+		for entity in self.entities:
+			if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+				return entity
+
+		return None
 		
 	def in_bounds(self, x: int, y: int) -> bool:
 		"""Return True if x and y are inside of the bounds of this map."""
